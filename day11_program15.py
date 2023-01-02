125b55d4913f0bae668f64663841ef53910a52d8
shaiksufiya19 <121159101+shaiksufiya19@users.noreply.github.com>
2023-01-02 16:53:17 +0530

    Add files via upload

---
+++ day11_program15.py
@@ -0,0 +1,12 @@
+def printPattern(ch,n):
+    assert(n>=0),'INVALID'
+    for i in range(n,0,-1):
+        print(ch*i)
+    
+ch=input()
+n=int(input())
+try:
+    printPattern(ch,n)
+except AssertionError as a:
+    print(a)
+        
