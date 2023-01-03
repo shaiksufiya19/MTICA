a379b1d1df12955fbecf8130ad0411de628ce5e6
shaiksufiya19 <121159101+shaiksufiya19@users.noreply.github.com>
2023-01-03 17:31:46 +0530

    Add files via upload

---
+++ day12_output04.py
@@ -0,0 +1,114 @@
+Python 3.11.0a5 (main, Feb  3 2022, 19:32:53) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: D:/Pythonpractice57/Day12/day12_program12.py ============
+length:4
+width:6
+Area of rectangle is 24
+Perimeter of rectangle is 20
+
+============= RESTART: D:/Pythonpractice57/Day12/day12_program12.py ============
+length:4
+width:-8
+Invalid
+
+============= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ============
+12
+Factorial of  12 is 479001600
+
+============= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ============
+5
+Factorial of  5 is 120
+
+============= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ============
+2
+Factorial of  2 is 2
+Traceback (most recent call last):
+  File "D:/Pythonpractice57/Day12/day12_program13.py", line 20, in <module>
+    print(inp,"is",obj.checkEven())
+AttributeError: 'Number' object has no attribute 'checkEven'. Did you mean: 'checkEvenOdd'?
+
+============= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ============
+2
+Traceback (most recent call last):
+  File "D:/Pythonpractice57/Day12/day12_program13.py", line 20, in <module>
+    print(inp,"is",obj.checkEven())
+AttributeError: 'Number' object has no attribute 'checkEven'. Did you mean: 'checkEvenOdd'?
+
+============= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ============
+34
+Factorial of  34 is 295232799039604140847618609643520000000
+34 is Even
+Sum of Digits Of 34 is 7
+
+========================= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ========================
+34
+Factorial of  34 is 295232799039604140847618609643520000000
+34 is Even
+Sum of Digits Of 34 is 7
+Traceback (most recent call last):
+  File "D:/Pythonpractice57/Day12/day12_program13.py", line 41, in <module>
+    print('ArmstrongNumber Of',inp,'is',obj.checkArmstrongNumber())
+  File "D:/Pythonpractice57/Day12/day12_program13.py", line 27, in checkArmstrongNumber
+    for i in num:
+NameError: name 'num' is not defined. Did you mean: 'sum'?
+
+========================= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ========================
+34
+Factorial of  34 is 295232799039604140847618609643520000000
+34 is Even
+Sum of Digits Of 34 is 7
+Traceback (most recent call last):
+  File "D:/Pythonpractice57/Day12/day12_program13.py", line 41, in <module>
+    print('ArmstrongNumber Of',inp,'is',obj.checkArmstrongNumber())
+  File "D:/Pythonpractice57/Day12/day12_program13.py", line 28, in checkArmstrongNumber
+    total +=int(i)**n
+ValueError: invalid literal for int() with base 10: '<'
+
+========================= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ========================
+34
+Factorial of  34 is 295232799039604140847618609643520000000
+34 is Even
+Sum of Digits Of 34 is 7
+Traceback (most recent call last):
+  File "D:/Pythonpractice57/Day12/day12_program13.py", line 42, in <module>
+    print('ArmstrongNumber Of',inp,'is',obj.checkArmstrongNumber())
+  File "D:/Pythonpractice57/Day12/day12_program13.py", line 26, in checkArmstrongNumber
+    l=len(self)
+TypeError: object of type 'Number' has no len()
+
+========================= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ========================
+46
+Factorial of  46 is 5502622159812088949850305428800254892961651752960000000000
+46 is Even
+Sum of Digits Of 46 is 10
+46 is Not Armstrong
+Not Prime
+
+========================= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ========================
+8
+Factorial of  8 is 40320
+8 is Armstrong
+Not Prime
+
+========================= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ========================
+-8
+Factorial of  -8 is 1
+The number should be >=0
+INVALID
+
+========================= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ========================
+28
+Factorial of  28 is 304888344611713860501504000000
+28 is Even
+Sum of Digits Of 28 is 10
+28 is Not Armstrong
+Not Prime
+
+========================= RESTART: D:/Pythonpractice57/Day12/day12_program13.py ========================
+-12
+Factorial of  -12 is 1
+-12 is Even
+Sum of Digits Of -12 is 3
+-12 is Not Armstrong
+Not Prime
--- day12_program13.py
+++ day12_program13.py
@@ -21,16 +21,24 @@ class Number:
         for i in temp:
             t+=int(i)
         return t
-    def checkArmstrongNumber(self):
-        n=str(self)
-        l=len(self)
-        total=0
-        for i in self:
-            total +=int(i)**n
-        if int(self)==total:
-            return 1
+    def checkArmstrong(self):
+        assert self.n>=0,'The number should be >=0'
+        temp=str(self.n)
+        t=0
+        for i in temp:
+            t+=int(i)**len(temp)
+        if t==self.n:
+            return 'Armstrong'
         else:
-            return 0
+            return 'Not Armstrong'
+    def checkPrime(self):
+        assert(self.n>=0),"INVALID"
+        if (self.n==1 or self.n==2 or self.n==3):
+            return "prime"
+        for i in range(2,self.n):
+            if self.n%i==0:
+                return "Not Prime"
+        return "prime"
     
     
     
@@ -39,5 +47,14 @@ obj=Number(inp)
 print('Factorial of ',inp,'is',obj.calculateFactorial())
 print(inp,"is",obj.checkEvenOdd())
 print('Sum of Digits Of',inp,'is',obj.sumOfDigits())
-print('ArmstrongNumber Of',inp,'is',obj.checkArmstrongNumber())
-        
+try:
+    print(inp,'is',obj.checkArmstrong())
+except AssertionError as a:
+    print(a)
+try:
+    prim=obj.checkPrime()
+    print(prim)
+except AssertionError as ob:
+    print(ob)
+
+
